32d93c1fd12133e6b9a024a3a2f7efcd0ecb94a0
Omer Hanetz <o.hanetz@gmail.com>
2016-04-21 01:54:17 +0300

    Added plane intersection.

--- Ex1/src/RayTracing/Plane.java
+++ Ex1/src/RayTracing/Plane.java
@@ -2,14 +2,32 @@ package RayTracing;
 
 public class Plane implements Surface{
 
-	private Point normal;
+	private Vector normal;
 	private double offset;
 	private int materialIndex;
 	
 	public Plane(String[] params)
 	{
-		normal = new Point(Double.parseDouble(params[0]), Double.parseDouble(params[1]), Double.parseDouble(params[2]));
+		normal = new Vector(Double.parseDouble(params[0]), Double.parseDouble(params[1]), Double.parseDouble(params[2]));
 		offset = Double.parseDouble(params[3]);
 		materialIndex = Integer.parseInt(params[4]);
+	}
+	
+	@Override
+	public double getIntersection(Ray ray) {
+		
+		double vd = VectorOperations.dotProduct(normal, ray.getV());
+		if (vd == 0) {
+			// Plane is parallel - no intersection.
+			return -1;
+		}
+		
+		double v0 = (-1) * VectorOperations.dotProduct(normal, VectorOperations.add(ray.getP0(), new Vector(offset, offset, offset)));
+		double t = v0 / vd;
+		if (t < 0) {
+			return -1;
+		}
+		
+		return t;
 	}
 }
