706ff8745d03a18c02691a77bcf634be4af4c875
Omer Hanetz <o.hanetz@gmail.com>
2016-04-25 01:19:52 +0300

    Added initial cylinder calculation. Still need to make sure how to go from center point to the calculated vector.

--- Ex1/src/RayTracing/Cylinder.java
+++ Ex1/src/RayTracing/Cylinder.java
@@ -1,11 +1,13 @@
 package RayTracing;
 
+
 public class Cylinder extends Surface{
 
 	private Point centerPosition;
 	private double length;
 	private double radius;
-	private Point rotation;
+	private Vector direction;
+	private Point base;
 	
 	public Cylinder(String[] params)
 	{
@@ -13,16 +15,57 @@ public class Cylinder extends Surface{
 		centerPosition = new Point(Double.parseDouble(params[0]), Double.parseDouble(params[1]), Double.parseDouble(params[2]));
 		length = Double.parseDouble(params[3]);
 		radius = Double.parseDouble(params[4]);
-		rotation = new Point(Double.parseDouble(params[5]), Double.parseDouble(params[6]), Double.parseDouble(params[7]));
+		double xRotation = Double.parseDouble(params[5]);
+		double yRotation = Double.parseDouble(params[6]);
+		double zRotation = Double.parseDouble(params[7]);
 		
+		direction = new Vector(0, 0, 1);
+		direction = MatrixRotation.rotateX(direction, xRotation);
+		direction = MatrixRotation.rotateY(direction, yRotation);
+		direction = MatrixRotation.rotateZ(direction, zRotation);
+		
+		base = VectorOperations.subtract(centerPosition, direction);
 	}
 
 
 
 	@Override
 	public double getIntersection(Ray ray) {
-		// TODO Auto-generated method stub
-		return 0;
+		// Preparing arguments for quadratic equation.
+		double vDP = VectorOperations.dotProduct(ray.getV(), direction);
+		Vector deltaP = VectorOperations.subtract(ray.getP0(), base);
+		double pDP = VectorOperations.dotProduct(deltaP, direction);
+		
+		Vector aV = VectorOperations.subtract(ray.getV(), VectorOperations.scalarMult(vDP, direction));
+		double a = aV.getLength() * aV.getLength();
+		
+		Vector bV = VectorOperations.subtract(deltaP, VectorOperations.scalarMult(pDP, direction));
+		double b = 2 * VectorOperations.dotProduct(aV, bV);
+		
+		double c = bV.getLength() * bV.getLength() - radius * radius;
+		
+		double[] t = solveQuadraticEquation(a, b, c);
+		
+		if (t == null) {
+			// No intersection.
+			return -1;
+			
+		} else if ((t[0] < 0) && (t[1] < 0)) {
+			// No intersection.
+			return -1;
+		
+		} else if ((t[0] >= 0) && (t[1] < 0)) {
+			// Only first solution is valid.
+			return t[0];
+			
+		} else if ((t[0] < 0) && (t[1] >= 0)) {
+			// Only second solution is valid.
+			return t[1];
+		
+		} else {
+			// Both solutions are valid.
+			return Math.min(t[0], t[1]);
+		}
 	}
 
 
@@ -31,5 +74,20 @@ public class Cylinder extends Surface{
 	public Vector getNormal(Vector lightDirection, Point point, boolean withDirection) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	
+	private double[] solveQuadraticEquation(double a, double b, double c) {
+		
+		double disc = b * b - 4 * a * c;
+		if (disc < 0) { 
+			return null;
+		}
+		
+		double[] res = new double[2];
+		res[0] = ((-1) * b + Math.sqrt(disc)) / (2 * a);
+		res[0] = ((-1) * b - Math.sqrt(disc)) / (2 * a);
+		
+		return res;
 	}
 }
---
+++ Ex1/src/RayTracing/MatrixRotation.java
@@ -0,0 +1,34 @@
+package RayTracing;
+
+public class MatrixRotation {
+
+	
+	public static Vector rotateX(Vector v, double theta) {
+		// Multiplying by rotation matrix.
+		return new Vector(
+				v.getX(), // Omitted zeroes.
+				Math.cos(theta) * v.getY() + (-1) * Math.sin(theta) * v.getZ(), // Omitted zero.
+				Math.sin(theta) * v.getY() + Math.cos(theta) * v.getZ() // Omitted zero.
+				);
+	}
+	
+	
+	public static Vector rotateY(Vector v, double theta) {
+		// Multiplying by rotation matrix.
+		return new Vector(
+				Math.cos(theta) * v.getX() + Math.sin(theta) * v.getZ(), // Omitted zero.
+				v.getY(), // Omitted zeroes.
+				(-1) * Math.sin(theta) * v.getX() + Math.cos(theta) * v.getZ() // Omitted zero.
+				);
+	}
+	
+	
+	public static Vector rotateZ(Vector v, double theta) {
+		// Multiplying by rotation matrix.
+		return new Vector(
+				Math.cos(theta) * v.getX() + (-1) * Math.sin(theta) * v.getY(), // Omitted zero.
+				Math.sin(theta) * v.getX() + Math.cos(theta) * v.getY(), // Omitted zero.
+				v.getZ() // Omitted zeroes.
+				);
+	}
+}
