309fb8ca0773ea3670a5f65e39365e2e4b8609fb
jwolbe <wolbe@mail.tau.ac.il>
2016-05-04 20:37:59 +0300

    Question 2 (UNFINISHED!)

---
+++ Ex2/DisplayCorr.m
@@ -0,0 +1,7 @@
+% TODO
+function [ displayedCorr ] = DisplayCorr( image1,image2,matches,dist_vals,x )
+
+
+
+end
+
---
+++ Ex2/match.m
@@ -0,0 +1,11 @@
+% TODO
+function [ num_matches,matches,dist_vals ] = match( image1,image2,distRatio )
+
+[im1, des1, loc1] = sift(image1);
+[im2, des2, loc2] = sift(image2);
+
+
+
+
+end
+
