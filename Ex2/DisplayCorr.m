1e7275c1b9a79a2707d0ce9dd7c3e82669e476fd
Omer Hanetz <o.hanetz@gmail.com>
2016-05-16 23:15:50 +0300

    Added question2 implementation.

--- Ex2/DisplayCorr.m
+++ Ex2/DisplayCorr.m
@@ -1,7 +1,39 @@
-% TODO
-function [ displayedCorr ] = DisplayCorr( image1,image2,matches,dist_vals,x )
-
-
-
+function [ displayedCorr ] = DisplayCorr( image1,  image2, matches, dist_vals, x )
+%DISPLAYCORR Summary of this function goes here
+%   Detailed explanation goes here
+    
+    % Making sure images has the same size.
+    im1_rows = size(image1, 1);
+    im1_cols = size(image1, 2);
+    im2_rows = size(image2, 1);
+    im2_cols = size(image2, 2);
+    if im1_rows >= im2_rows
+        im1 = image1;
+        im2 = cat(1, image2, zeroes(im1_rows - im2_rows), im2_cols);
+    else
+        im1 = cat(1, image1, zeroes(im2_rows - im1_rows), im1_cols);
+        im2 = image2;
+    end
+    
+    [dv_sorted, dv_indices] = sort(dist_vals);
+    
+    displayedCorr = [];
+    x = min(x, size(matches, 1));
+    % Preparing images for display with correlation indication, and building displayedCorr.
+    for i = 1 : x
+        match = matches(dv_indices(i), :);
+        displayedCorr = [displayedCorr; match];
+        
+        im1_match = match(1:2);
+        im2_match = match(3:4);
+        % Modifying images to show match indication.
+        im1 = insertText(im1, im1_match, i);
+        im2 = insertText(im2, im2_match, i);
+    end
+    
+    
+    final_image = [im1 im2];
+    imshow(final_image);
+    
 end
 
--- Ex2/match.m
+++ Ex2/match.m
@@ -1,11 +1,33 @@
-% TODO
-function [ num_matches,matches,dist_vals ] = match( image1,image2,distRatio )
+function [num_matches, matches, dist_vals] = match(image1, image2, distRatio)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
 
-[im1, des1, loc1] = sift(image1);
-[im2, des2, loc2] = sift(image2);
+	% Find SIFT keypoints for each image
+	[im1, des1, loc1] = sift(image1);
+	[im2, des2, loc2] = sift(image2);
 
+    % num_matches = 0;
+    matches = [];
+	dist_vals = [];
+	% For each descriptor in the first image, select its match to second image.
+	des2t = des2';                          % Precompute matrix transpose
+	for i = 1 : size(des1,1)
+	   dotprods = des1(i,:) * des2t;        % Computes vector of dot products
+	   [vals,indx] = sort(acos(dotprods));  % Take inverse cosine and sort results
 
+	   % Check if nearest neighbor has angle less than distRatio times 2nd.
+	   if (vals(1) < distRatio * vals(2))
+	      % match(i) = indx(1);
+	      dist_vals = [dist_vals; vals(1)];
+          % match = [loc1(i, 1:2), loc2(indx(1), 1:2)];
+          match = [fliplr(loc1(i, 1:2)), fliplr(loc2(indx(1), 1:2))];
+          matches = [matches; match];
+          % num_matches = num_matches + 1;
+	   else
+	      % match(i) = 0;
+	   end
+	end
 
-
+	% num_matches = sum(match > 0);
+    num_matches = size(matches, 1);
 end
-
