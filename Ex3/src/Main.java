e4edd31dda82eb179067ae23b6da624a7ba1d1c9
jwolbe <wolbe@mail.tau.ac.il>
2016-05-21 19:10:51 +0300

    Energy map (and entropy) completed

--- Ex3/src/Main.java
+++ Ex3/src/Main.java
@@ -2,6 +2,8 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.*;
 
@@ -12,7 +14,7 @@ public class Main {
 		String inputImagePath = args[0];
 		int numOutputColumns = Integer.parseInt(args[1]);
 		int numOutputRows = Integer.parseInt(args[2]);
-		int energyType = Integer.parseInt(args[3]); //TODO boolean or int?
+		int energyType = Integer.parseInt(args[3]); 
 		String outputImagePath = args[4];
 		
 		BufferedImage inputImage = null;
@@ -24,12 +26,12 @@ public class Main {
 		}
 		
 		// step 1
-		int[][] energyValues = computeEnergyFunction(inputImage);
+		int[][] energyValues = computeEnergyMap(inputImage, energyType);
 	
 		
 	}
 
-	private static int[][] computeEnergyFunction(BufferedImage inputImage) {
+	private static int[][] computeEnergyMap(BufferedImage inputImage, int energyType) {
 		int inputImageHeight = inputImage.getHeight();
 		int inputImageWidth = inputImage.getWidth();
 		
@@ -41,32 +43,95 @@ public class Main {
 			{
 				int pixelGradient = computePixelGradient(inputImage,i,j);
 				energyValues[i][j] = pixelGradient;
-				//TODO steps 2 & 3: local entropy
+				if (energyType == 1)
+				{
+					energyValues[i][j] += getEntropyValue(inputImage,i,j);
+				}
 			}
 		}
 		
 		return energyValues;
 	}
 
-	
-	
-	
-	// TODO change implementation according to page 3 in the instructions 
-	private static int computePixelGradient(BufferedImage inputImage, int i, int j) {
-		int currPixelValue = getSummedRGBValue(inputImage,i,j);
-		
-		return 8*currPixelValue - (getSummedRGBValue(inputImage,i-1,j-1)+getSummedRGBValue(inputImage,i,j-1)
-		+getSummedRGBValue(inputImage,i+1,j-1)+getSummedRGBValue(inputImage,i+1,j)+getSummedRGBValue(inputImage,i+1,j+1)
-		+getSummedRGBValue(inputImage,i,j+1)+getSummedRGBValue(inputImage,i-1,j+1));	
+	private static int getEntropyValue(BufferedImage inputImage, int i, int j) {
+		int sum = 0;
+		for (int n = i-4; n < i+4; n++)
+		{
+			for (int m = j-4; m < j+4; m++)
+			{
+				int pm = getPmn(inputImage,i,j);
+				sum += pm * log2(pm); //TODO base 2 indeed?
+			}
+		}
+		return -1*sum;
 	}
 
-	private static int getSummedRGBValue(BufferedImage inputImage, int i, int j) {
+	private static int getPmn(BufferedImage inputImage, int i, int j) {
+		int fmn = getGrayscaleValue(inputImage,i,j);
+		int sum = 0;
+		for (int l = i-4; l < i+4; l++)
+		{
+			for (int k = j-4; k < j+4; k++)
+			{
+				sum += getGrayscaleValue(inputImage,i,j);
+			}
+		}
+		return fmn/sum;
+	}
+
+	private static int getGrayscaleValue(BufferedImage inputImage, int i, int j) {
+		Color color = new Color(inputImage.getRGB(i, j));
+		return (color.getRed()+color.getGreen()+color.getBlue())/3;
+	}
+
+	private static double log2(int n) {
+		return Math.log(n)/Math.log(2);
+	}
+
+	private static int computePixelGradient(BufferedImage inputImage, int i, int j) {
+		int energy = 0;
+		int numNeighbours = 0;
+		List<Integer> neighboursValues = getNeighboursValues(inputImage,i,j);
+		for (int neighbourValue : neighboursValues)
+		{
+			if (neighbourValue != -1)
+			{
+				energy += neighbourValue;
+				numNeighbours++;
+			}
+
+		}
+		return energy/numNeighbours;
+	}
+
+	
+	private static List<Integer> getNeighboursValues(BufferedImage inputImage, int i, int j) {
+		Color inputPixel = new Color(inputImage.getRGB(i, j));
+		List<Integer> neighboursValues = new ArrayList<>();
+		
+		neighboursValues.add(getValue(inputImage,i-1,j-1,inputPixel));
+		neighboursValues.add(getValue(inputImage,i-1,j,inputPixel));
+		neighboursValues.add(getValue(inputImage,i-1,j+1,inputPixel));
+		neighboursValues.add(getValue(inputImage,i,j+1,inputPixel));
+		neighboursValues.add(getValue(inputImage,i+1,j+1,inputPixel));
+		neighboursValues.add(getValue(inputImage,i+1,j,inputPixel));
+		neighboursValues.add(getValue(inputImage,i+1,j-1,inputPixel));
+		neighboursValues.add(getValue(inputImage,i,j-1,inputPixel));
+
+		return neighboursValues;
+	}
+
+	private static int getValue(BufferedImage inputImage, int i, int j, Color inputPixel)
+	{
 		if (i < 0 || j < 0 || i >= inputImage.getHeight() || j >= inputImage.getWidth())
 		{
-			return 0;
+			return -1;
 		}
+		Color currPixel = new Color(inputImage.getRGB(i, j));
 		
-		Color color = new Color(inputImage.getRGB(i, j));
-		return color.getRed()+color.getGreen()+color.getBlue();
+		return Math.abs(inputPixel.getRed()-currPixel.getRed()) + Math.abs(inputPixel.getGreen()-currPixel.getGreen()) + Math.abs(inputPixel.getBlue()-currPixel.getBlue());
 	}
+	
+	
+
 }
